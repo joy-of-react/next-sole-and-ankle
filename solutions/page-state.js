6c514bef8923d8ad154c3bf682527341dd4c361d
Joshua Comeau <joshwcomeau@gmail.com>
2023-06-12 17:38:19 -0400

    Tweak “solution” code

--- solutions/page-state.js
+++ solutions/page-state.js
@@ -1,3 +1,14 @@
+/**
+ * This shows a sketch of a potential implementation using
+ * React state. We use a client component that performs a
+ * `fetch` operation inside a useEffect hook.
+ *
+ * Note that I would not suggest doing it this way. I'd suggest
+ * either using the modern Suspense approach discussed in the
+ * lesson, or the SWR approach shown in the next file
+ * (`page-swr.js`)
+ */
+'use client';
 import React from 'react';
 
 import { getShoesForCategory } from '@/helpers/data';
@@ -9,11 +20,12 @@ function CategoryPage({ params }) {
 
   React.useEffect(() => {
     async function runEffect() {
-      const data = await getShoesForCategory(
-        params.categorySlug
+      const response = await fetch(
+        `/api/shoes?categorySlug=${params.categorySlug}`
       );
+      const data = await response.json();
 
-      setShoes(data);
+      setShoes(data.shoes);
       setIsLoading(false);
     }
 
--- solutions/page-swr.js
+++ solutions/page-swr.js
@@ -1,3 +1,10 @@
+/**
+ * This shows a sketch of a potential implementation using
+ * SWR. It makes a request to the /api/shoes endpoint.
+ *
+ * This is a perfectly valid option, though it doesn't perform
+ * quite as nicely as the modern alternative using Suspense.
+ */
 'use client';
 import React from 'react';
 import useSWR from 'swr';
